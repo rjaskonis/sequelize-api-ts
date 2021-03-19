e6f8a961755264b1cd1bac847236a9648543b09a
Renne Jaskonis <rjaskonis@gmail.com>
2021-03-19 11:10:24 -0300

    Set data layer interface

---
+++ src/data/repository.ts
@@ -0,0 +1,18 @@
+import { Entity } from "@/domain/entity";
+import { Interactor } from "@domain/interactor";
+
+export default interface Repository {
+    execute?: (command: string) => Promise<void>;
+
+    query?: (command: string) => Promise<any>;
+
+    read?: (path: string) => Promise<any>;
+
+    store?: (param: Entity | Interactor.Data) => Promise<any>;
+
+    delete?: (param: Entity | Interactor.Data) => Promise<any>;
+
+    findAll?: () => Promise<Array<any>>;
+
+    findOne?: (lambda: any) => Promise<any>;
+}
