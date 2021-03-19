d7724f8b485f16ef03147b7f9ca203029007ba04
Renne Jaskonis <rjaskonis@gmail.com>
2021-03-19 11:10:01 -0300

    Set domain layer main interfaces

---
+++ src/domain/interactor.ts
@@ -0,0 +1,15 @@
+import { Entity } from "@domain/entity";
+
+export default interface Interactor {
+    query: (command: string) => Promise<any>;
+
+    findAll: () => Promise<Array<Entity>>;
+
+    findOne: (param: any) => Promise<any>;
+
+    store: (data: Entity | Interactor.Data | any) => Promise<void>;
+}
+
+export namespace Interactor {
+    export type Data = Omit<Entity, "id">;
+}
