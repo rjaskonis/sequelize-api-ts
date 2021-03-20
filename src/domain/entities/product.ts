e8efe4d160e3252c617e788648e4b8c129d98e20
Renne Jaskonis <rjaskonis@gmail.com>
2021-03-20 18:26:08 -0300

    Changed entity fields order

--- src/domain/entities/product.ts
+++ src/domain/entities/product.ts
@@ -2,13 +2,14 @@ import { Entity } from "@domain/entity";
 
 export default class Product implements Entity {
     public id: Number;
+    public _deleted?: boolean;
 
     constructor(
+        public title: string,
         public description: string,
         public price: number,
-        public title: string,
         public manufacturer: string,
         public inventory: number,
-        public photo?: Buffer | any
+        public photo?: Buffer | undefined
     ) {}
 }
