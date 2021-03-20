7c74fbc77a83b645e1ddbbc320c315afd2d8d12d
Renne Jaskonis <rjaskonis@gmail.com>
2021-03-20 18:32:42 -0300

    Feat: products API

---
+++ src/infrastructure/http/controllers/product.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from "express";
+import Product from "@domain/entities/product";
+import ProductInteractor from "@domain/usecases/product.interactor";
+import SequelizeAdapter from "@data/adapters/sequelize";
+import productSchemaModel from "@infrastructure/database/schema/models/product";
+
+let interactor: ProductInteractor;
+
+export async function findProducts(req: Request, res: Response) {
+    const repository = new SequelizeAdapter(req.app.get("DATABASE_CONNECTION"), productSchemaModel);
+
+    interactor = interactor || new ProductInteractor(repository);
+
+    const { limit, offset } = req.query;
+
+    const products = await interactor.findAll({ limit, offset });
+
+    return res.json(products);
+}
+
+export async function findProductById(req: Request, res: Response) {
+    const repository = new SequelizeAdapter(req.app.get("DATABASE_CONNECTION"), productSchemaModel);
+
+    interactor = interactor || new ProductInteractor(repository);
+
+    const product = await interactor.findOne({ where: { id: req.params.id } });
+
+    return res.json(product);
+}
+
+export async function createProduct(req: Request, res: Response) {
+    const repository = new SequelizeAdapter(req.app.get("DATABASE_CONNECTION"), productSchemaModel);
+
+    interactor = interactor || new ProductInteractor(repository);
+
+    const product = req.body;
+
+    try {
+        const createdProduct = await interactor.store(
+            new Product(product.title, product.description, product.price, product.manufacturer, product.inventory, product.photo)
+        );
+
+        return res.status(201).json(createdProduct);
+    } catch (err) {
+        return res.sendStatus(400);
+    }
+}
+
+export async function updateProduct(req: Request, res: Response) {
+    const repository = new SequelizeAdapter(req.app.get("DATABASE_CONNECTION"), productSchemaModel);
+
+    interactor = interactor || new ProductInteractor(repository);
+
+    try {
+        const registeredProduct = await interactor.findOne({ where: { id: req.params.id } });
+
+        if (!registeredProduct) return res.status(400).send(`Product ${req.params.id} not registered`);
+
+        const product = req.body;
+
+        await interactor.store({ ...registeredProduct, ...product });
+
+        return res.sendStatus(204);
+    } catch (err) {
+        return res.sendStatus(400);
+    }
+}
+
+export async function deleteProduct(req: Request, res: Response) {
+    const repository = new SequelizeAdapter(req.app.get("DATABASE_CONNECTION"), productSchemaModel);
+
+    interactor = interactor || new ProductInteractor(repository);
+
+    try {
+        const registeredProduct = await interactor.findOne({ where: { id: req.params.id } });
+
+        if (!registeredProduct) return res.status(400).send(`Product ${req.params.id} not registered`);
+
+        await interactor.delete({ where: { id: registeredProduct.id } });
+
+        return res.sendStatus(200);
+    } catch (err) {
+        console.log(err);
+
+        return res.sendStatus(400);
+    }
+}
--- src/infrastructure/http/routes/product.ts
+++ src/infrastructure/http/routes/product.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { storeProduct, deleteProduct, findAllProducts, findProductById } from "@http/controllers/product";
+import { findProducts, findProductById, createProduct, updateProduct, deleteProduct } from "@http/controllers/product";
 
 const router = Router();
 
-router.get("/api/users", findAllProducts);
-router.get("/api/users/:id", findProductById);
-router.post("/api/users", storeProduct);
-router.delete("/api/users", deleteProduct);
+router.get("/api/products", findProducts);
+router.get("/api/products/:id", findProductById);
+router.post("/api/products", createProduct);
+router.put("/api/products/:id", updateProduct);
+router.delete("/api/products/:id", deleteProduct);
 
 export default router;
